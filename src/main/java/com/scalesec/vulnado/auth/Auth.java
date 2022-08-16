63004b437df89b94936794d555e8820cbf5f5645
Neil Pathare <neil.pathare@outlook.com>
2022-08-16 08:36:39 -0400

    Update Auth.java

--- src/main/java/com/scalesec/vulnado/auth/Auth.java
+++ src/main/java/com/scalesec/vulnado/auth/Auth.java
@@ -8,5 +8,5 @@ public class Auth {
   private String password = "password";
   private String jwt_token = "ey09sdh38r3r39r3r93rj39rj3r9hr39rh";
   private String username = "admin";
-  
+  private String newPass = "abc2daslkfjasdolvma";
 }
